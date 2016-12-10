69952e53820946cd0fa424002574e4eb688c4307
Rocket-Fish <rocket.fish@mail.com>
2016-12-09 19:19:27 -0500

    Initial Files

---
+++ desktop/src/com/bearfishapps/shadowarcher/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bearfishapps.shadowarcher.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bearfishapps.shadowarcher.MainGameClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGameClass(), config);
+	}
+}
